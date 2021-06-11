6786ce4bf9afac1a1d24aebe0c8fc7e067f4aded
Dalibor989 <lazic_dalibor@hotmail.com>
2021-06-11 20:16:08 +0200

    zadatak 2

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Vivify Blog</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="styles/blog.css" rel="stylesheet">
+</head>
+<body>
+  
+  <?php include_once("header.php") ?>
+
+  <?php include_once("posts.php") ?> 
+
+  <?php include_once("sidebar.php") ?>
+  
+  <?php include_once("footer.php") ?>
+  
+</body>
+</html>
